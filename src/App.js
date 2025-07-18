fd3b4208812a43bd0c28dda810b22a59158318c1
Techno2134 <137487665+Techno2134@users.noreply.github.com>
2025-07-18 07:41:21 +0000

    App js

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+import './styles/main.css'; // your custom animation styles
+import Home from './pages/Home';
+
+function App() {
+  return <Home />;
+}
+
+export default App;
